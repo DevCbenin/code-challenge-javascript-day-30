5563ae198804cdc915a67fe39ff543194714f5a2
Damilare <32745404+veectorh@users.noreply.github.com>
2020-06-10 13:42:16 +0100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,226 +1,89 @@
 
 
-const ComplexNumber = require('./index');
+const BinarySearchTree = require('./index');
 
-describe('Complex numbers', () => {
-  test('Real part of a purely real number', () => {
-    const expected = 1;
-    const actual = new ComplexNumber(1, 0).real;
+function recordAllData(bst) {
+  const out = [];
 
-    expect(actual).toEqual(expected);
+  bst.each(data => out.push(data));
+
+  return out;
+}
+
+describe('BinarySearchTree', () => {
+  test('data is retained', () => {
+    expect(new BinarySearchTree(4).data).toEqual(4);
   });
 
-  xtest('Real part of a purely imaginary number', () => {
-    const expected = 0;
-    const actual = new ComplexNumber(0, 1).real;
+  test('inserting less', () => {
+    const four = new BinarySearchTree(4);
+    four.insert(2);
 
-    expect(actual).toEqual(expected);
+    expect(four.data).toEqual(4);
+    expect(four.left.data).toEqual(2);
   });
 
-  xtest('Real part of a number with real and imaginary part', () => {
-    const expected = 1;
-    const actual = new ComplexNumber(1, 2).real;
+  test('inserting same', () => {
+    const four = new BinarySearchTree(4);
+    four.insert(4);
 
-    expect(actual).toEqual(expected);
+    expect(four.data).toEqual(4);
+    expect(four.left.data).toEqual(4);
   });
 
-  xtest('Imaginary part of a purely real number', () => {
-    const expected = 0;
-    const actual = new ComplexNumber(1, 0).imag;
+  test('inserting right', () => {
+    const four = new BinarySearchTree(4);
+    four.insert(5);
 
-    expect(actual).toEqual(expected);
+    expect(four.data).toEqual(4);
+    expect(four.right.data).toEqual(5);
   });
 
-  xtest('Imaginary part of a purely imaginary number', () => {
-    const expected = 1;
-    const actual = new ComplexNumber(0, 1).imag;
+  test('complex tree', () => {
+    const four = new BinarySearchTree(4);
+    four.insert(2);
+    four.insert(6);
+    four.insert(1);
+    four.insert(3);
+    four.insert(7);
+    four.insert(5);
 
-    expect(actual).toEqual(expected);
+    expect(four.data).toEqual(4);
+    expect(four.left.data).toEqual(2);
+    expect(four.left.left.data).toEqual(1);
+    expect(four.left.right.data).toEqual(3);
+    expect(four.right.data).toEqual(6);
+    expect(four.right.left.data).toEqual(5);
+    expect(four.right.right.data).toEqual(7);
   });
 
-  xtest('Imaginary part of a number with real and imaginary part', () => {
-    const expected = 2;
-    const actual = new ComplexNumber(1, 2).imag;
-
-    expect(actual).toEqual(expected);
+  test('iterating one element', () => {
+    expect(recordAllData(new BinarySearchTree(4))).toEqual([4]);
   });
 
-  xtest('Add purely real numbers', () => {
-    const expected = new ComplexNumber(3, 0);
-    const actual = new ComplexNumber(1, 0).add(new ComplexNumber(2, 0));
+  test('iterating over smaller element', () => {
+    const four = new BinarySearchTree(4);
+    four.insert(2);
 
-    expect(actual).toEqual(expected);
+    expect(recordAllData(four)).toEqual([2, 4]);
   });
 
-  xtest('Add purely imaginary numbers', () => {
-    const expected = new ComplexNumber(0, 3);
-    const actual = new ComplexNumber(0, 1).add(new ComplexNumber(0, 2));
+  test('iterating over larger element', () => {
+    const four = new BinarySearchTree(4);
+    four.insert(5);
 
-    expect(actual).toEqual(expected);
+    expect(recordAllData(four)).toEqual([4, 5]);
   });
 
-  xtest('Add numbers with real and imaginary part', () => {
-    const expected = new ComplexNumber(4, 6);
-    const actual = new ComplexNumber(1, 2).add(new ComplexNumber(3, 4));
+  test('iterating over complex tree', () => {
+    const four = new BinarySearchTree(4);
+    four.insert(2);
+    four.insert(1);
+    four.insert(3);
+    four.insert(6);
+    four.insert(7);
+    four.insert(5);
 
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Subtract purely real numbers', () => {
-    const expected = new ComplexNumber(-1, 0);
-    const actual = new ComplexNumber(1, 0).sub(new ComplexNumber(2, 0));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Subtract purely imaginary numbers', () => {
-    const expected = new ComplexNumber(0, -1);
-    const actual = new ComplexNumber(0, 1).sub(new ComplexNumber(0, 2));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Subtract numbers with real and imaginary part', () => {
-    const expected = new ComplexNumber(-2, -2);
-    const actual = new ComplexNumber(1, 2).sub(new ComplexNumber(3, 4));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Multiply purely real numbers', () => {
-    const expected = new ComplexNumber(2, 0);
-    const actual = new ComplexNumber(1, 0).mul(new ComplexNumber(2, 0));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Multiply imaginary unit', () => {
-    const expected = new ComplexNumber(-1, 0);
-    const actual = new ComplexNumber(0, 1).mul(new ComplexNumber(0, 1));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Multiply purely imaginary numbers', () => {
-    const expected = new ComplexNumber(-2, 0);
-    const actual = new ComplexNumber(0, 1).mul(new ComplexNumber(0, 2));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Multiply numbers with real and imaginary part', () => {
-    const expected = new ComplexNumber(-5, 10);
-    const actual = new ComplexNumber(1, 2).mul(new ComplexNumber(3, 4));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Divide purely real numbers', () => {
-    const expected = new ComplexNumber(0.5, 0);
-    const actual = new ComplexNumber(1, 0).div(new ComplexNumber(2, 0));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Divide purely imaginary numbers', () => {
-    const expected = new ComplexNumber(0.5, 0);
-    const actual = new ComplexNumber(0, 1).div(new ComplexNumber(0, 2));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Divide numbers with real and imaginary part', () => {
-    const expected = new ComplexNumber(0.44, 0.08);
-    const actual = new ComplexNumber(1, 2).div(new ComplexNumber(3, 4));
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Absolute value of a positive purely real number', () => {
-    const expected = 5;
-    const actual = new ComplexNumber(5, 0).abs;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Absolute value of a negative purely real number', () => {
-    const expected = 5;
-    const actual = new ComplexNumber(-5, 0).abs;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Absolute value of a purely imaginary number with positive imaginary part', () => {
-    const expected = 5;
-    const actual = new ComplexNumber(0, 5).abs;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Absolute value of a purely imaginary number with negative imaginary part', () => {
-    const expected = 5;
-    const actual = new ComplexNumber(0, -5).abs;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Absolute value of a number with real and imaginary part', () => {
-    const expected = 5;
-    const actual = new ComplexNumber(3, 4).abs;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Conjugate a purely real number', () => {
-    const expected = new ComplexNumber(5, 0);
-    const actual = new ComplexNumber(5, 0).conj;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Conjugate a purely imaginary number', () => {
-    const expected = new ComplexNumber(0, -5);
-    const actual = new ComplexNumber(0, 5).conj;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Conjugate a number with real and imaginary part', () => {
-    const expected = new ComplexNumber(1, -1);
-    const actual = new ComplexNumber(1, 1).conj;
-
-    expect(actual).toEqual(expected);
-  });
-
-  xtest('Euler\'s identity/formula', () => {
-    const expected = new ComplexNumber(-1, 0);
-    const actual = new ComplexNumber(0, Math.PI).exp;
-
-    expect(actual.real).toBeCloseTo(expected.real);
-    expect(actual.imag).toBeCloseTo(expected.imag);
-  });
-
-  xtest('Exponential of 0', () => {
-    const expected = new ComplexNumber(1, 0);
-    const actual = new ComplexNumber(0, 0).exp;
-
-    expect(actual.real).toBeCloseTo(expected.real);
-    expect(actual.imag).toBeCloseTo(expected.imag);
-  });
-
-  xtest('Exponential of a purely real number', () => {
-    const expected = new ComplexNumber(Math.E, 0);
-    const actual = new ComplexNumber(1, 0).exp;
-
-    expect(actual.real).toBeCloseTo(expected.real);
-    expect(actual.imag).toBeCloseTo(expected.imag);
-  });
-
-  xtest('Exponential of a number with real and imaginary part', () => {
-    const expected = new ComplexNumber(-2, 0);
-    const actual = new ComplexNumber(Math.LN2, Math.PI).exp;
-
-    expect(actual.real).toBeCloseTo(expected.real);
-    expect(actual.imag).toBeCloseTo(expected.imag);
+    expect(recordAllData(four)).toEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 });
